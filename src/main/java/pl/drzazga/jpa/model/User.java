d5e5c86dfe7f434968374576a37e877ea6507566
drzazga <bartek4674@gmail.com>
2016-10-24 22:14:31 +0200

    add some tests

--- src/main/java/pl/drzazga/jpa/model/User.java
+++ src/main/java/pl/drzazga/jpa/model/User.java
@@ -5,12 +5,21 @@ import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.ManyToMany;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+import javax.validation.constraints.NotNull;
 
 @Entity
-@Table(name = "users")
+@Table(name = "users", 
+		uniqueConstraints = @UniqueConstraint(columnNames = { "email" }))
+@NamedQueries(value = {
+		@NamedQuery(name = "byEmail", query = "select u from User u where u.email = :email")
+})
 public class User extends BaseEntity {
-
+	
+	@NotNull
 	private String email;
 	
 	private String password;
@@ -24,6 +33,13 @@ public class User extends BaseEntity {
 	@ManyToMany
 	private List<Project> projects;
 
+	protected User() {
+	}
+
+	public User(String email) {
+		this.email = email;
+	}
+	
 	public String getEmail() {
 		return email;
 	}
--- src/test/java/pl/drzazga/jpa/model/UserTest.java
+++ src/test/java/pl/drzazga/jpa/model/UserTest.java
@@ -1,6 +1,10 @@
 package pl.drzazga.jpa.model;
 
+import java.util.Optional;
+
 import javax.persistence.EntityManager;
+import javax.persistence.OptimisticLockException;
+import javax.persistence.RollbackException;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -11,9 +15,43 @@ public class UserTest {
 
 	@Test
 	public void test() {
-		User user = new User();
+		User user = new User("test0@drzazga.pl");
 		persist(user);
 		Assert.assertNotNull(user.getId());
+	}
+	
+	@Test
+	public void RegularUser_WhenGetByEmail_GetsOne() {
+		String email = "test2@drzazga.pl";
+		User user1 = new User(email);
+		persist(user1);
+		Optional<User> user2 = getByEmail(email);
+		Assert.assertTrue(user2.isPresent());
+	}
+	
+	@Test(expected = OptimisticLockException.class)
+	public void TwoTheSameUsers_WhenMergeOneByOne_ThrowsOptimisticLockException() {
+		String email = "test3@drzazga.pl";
+		User user = new User(email);
+		persist(user);
+		
+		User user2 = getByEmail(email).orElse(new User());
+		User user3 = getByEmail(email).orElse(new User());
+		
+		user2.setFirstName("John");
+		user3.setFirstName("George");
+		
+		merge(user2);
+		merge(user3);
+	}
+	
+	@Test(expected = RollbackException.class)
+	public void UsersWithTheSameEmail_WhenPersist_ThorwsConstraintViolationException() {
+		User user1 = new User("test@drzazga.pl");
+		User user2 = new User("test@drzazga.pl");
+		
+		persist(user1);
+		persist(user2);
 	}
 	
 	/*@Test
@@ -30,8 +68,8 @@ public class UserTest {
 	public void UserWithRequiresData_WhenPersist_NoError() {
 		persist(new User("email@email.com", "password"));
 	}
-	
 	@Test(expected = RollbackException.class)
+	
 	public void UserWithEmptyEmail_WhenPersist_ThrowsException() {
 		persist(new User("", "password"));
 	}
@@ -47,10 +85,31 @@ public class UserTest {
 	}
 	*/
 	
+	public Optional<User> getByEmail(String email) {
+		EntityManager em = PersistenceManager.INSTANCE.getEntityManager();
+		em.getTransaction().begin();
+		Optional<User> user = em.createNamedQuery("byEmail", User.class)
+								.setParameter("email", email)
+								.getResultList()
+								.stream()
+								.findFirst();
+		em.getTransaction().commit();
+		em.close();
+		return user;
+	}
+	
 	private void persist(User user) {
 		EntityManager em = PersistenceManager.INSTANCE.getEntityManager();
 		em.getTransaction().begin();
 		em.persist(user);
+		em.getTransaction().commit();
+		em.close();
+	}
+	
+	private void merge(User user) {
+		EntityManager em = PersistenceManager.INSTANCE.getEntityManager();
+		em.getTransaction().begin();
+		em.merge(user);
 		em.getTransaction().commit();
 		em.close();
 	}
